f15482cd07a7e13bde8cec5074fcf763569252c7
byungjung <byungjung.kim@gmail.com>
2013-11-29 14:23:24 +0900

    move all example-xxx.js to example directory

---
+++ example/example-full.js
@@ -0,0 +1,55 @@
+'use strict';
+var connect = require('sensorjs'),
+   sensor = require('sensorjs').sensor;
+
+var app = connect().
+  use(connect.query({$between: [-50, 50]})). // filter: passing between -50 and 50
+  use(connect.average(5 /*duration*/)).      // reduce: values to an average every 5 sec.
+  use(connect.queue(100)).                   // buffering max # of 100.
+  use(function (data, next) {                // custom middleware
+    if (Math.max.apply(null, data.queue) < data.value) {
+      data.hint = 'new record';
+    } 
+    next();
+  }).
+  use(function (data, next) {
+    if (data.driver === 'ds18b20') {
+      if (data.value > 30) {
+        data.message = 'too hot';  
+      }
+      next();
+    } else if (data.driver === 'dht11') {
+      if (data.value < 20) {
+        data.message = 'too dry';  
+      }
+      next();
+    } else {
+      next(new Error('this will not connect to websocket below'));
+    }
+  }).
+  // transport(mqtt, localStorage, websocket and etc)
+  use(connect.websocket('http://yourhost.com', 'temperature/{id}'/*topic*/));
+
+sensor.discover('oneWire'/*sensor network*/, function (err, ids) {
+  ids.forEach(function (id) {
+    var thermometer = sensor.createSensor('ds18b20', id);
+
+    // listen to sensor data for connecting 
+    app.listen(thermometer);
+    
+    // or simply get sensor data once
+    // thermometer.get(function (err, data) {
+    //   if (!err) {
+    //     console.log(data);
+    //   } 
+    // });
+    // or in case of actuator
+    // someActuator.set(data, function (err) {});
+  });
+});
+sensor.discover('gpio', function (err, ids) {
+  ids.forEach(function (id) {
+    var hygrometer = sensor.createSensor('dht11', id);
+    app.listen(hygrometer);
+  });
+});
