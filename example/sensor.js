03ca5eb3dc15f03048ba127d2fbf56d14bb4a088
authere <authere@gmail.com>
2013-11-29 11:13:20 +0900

    mqtt -> websocket app -> temperatureApp

--- example/sensor.js
+++ example/sensor.js
@@ -2,7 +2,7 @@
 var sense = require('sensorjs').sense,
     sensor = require('sensorjs').sensor;
 
-var app = sense().
+var temperatureApp = sense().
   use(sense.query({$between: [-50, 50]})). // filter: passing between -50 and 50
   use(sense.average(5 /*duration*/)).      // reduce: values to an average every 5 sec.
   use(sense.queue(100)).                   // buffering max # of 100.
@@ -12,12 +12,12 @@ var app = sense().
     } 
     next();
   }).
-  //transport(mqtt, localStorage and etc)
-  use(sense.mqtt('yourhost.com'/*host*/, 'temperature/{id}'/*topic*/));
+  //transport(mqtt, localStorage, websocket and etc)
+  use(sense.websocket('http://yourhost.com', 'temperature/{id}'/*topic*/));
 
 sensor.discover('oneWire'/*sensor network*/, function (err, ids) {
   ids.forEach(function (id) {
-    app.listen(sensor.createSensor('ds18b20', id));
+    temperatureApp.listen(sensor.createSensor('ds18b20', id));
   });
 });
 
