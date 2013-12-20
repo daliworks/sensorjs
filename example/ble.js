05d8a1992c5d6dd4560f6b4b227e86ac9a498f61
authere <authere@gmail.com>
2013-12-20 20:26:56 +0900

    adding ble sensor

--- example/ble.js
+++ example/ble.js
@@ -1,33 +1,25 @@
 'use strict';
-var connect = require('sensorjs'),
-   sensor = connect.sensor,
-   ble = sensor.createNetwork('ble');
+var connect = require('../'),
+   sensorDriver = connect.sensor,
+   ble = sensorDriver.getNetwork('ble');
 
 var app = connect().
   use(connect.filter({$between: [-50, 50]})). // filter: passing between -50 and 50
-  use(connect.average(5 /*duration*/)).      // reduce: values to an average every 5 sec.
-  use(connect.queue(100)).                   // buffering max # of 100.
+  use(connect.average(20 /*duration*/)).      // reduce: values to an average every 5 sec.
   use(function (data, next) {                // custom middleware
     if (Math.max.apply(null, data.queue) < data.value) {
-      data.hint = 'new record';
+      console.log('new record', data.value);
     } 
     next();
   }).
+  use(connect.queue(100));                   // buffering max # of 100.
   // transport(mqtt, localStorage, websocket and etc)
-  use(connect.websocket('http://yourhost.com', 'temperature/{id}'/*topic*/));
+  //use(connect.websocket('http://yourhost.com', 'temperature/{id}'/*topic*/));
 
-ble.discover('PXP'/* sensor driver name(or profile name) */, function (err, devices) {
+ble.discover('sensorTagHum'/* sensor driver name(or profile name) */, function (err, devices) {
   devices.forEach(function (device) {
-    if (device.sensors) {
-      device.connect();
-      var thermometer = device.createSensor(device.sensors[0] );
-
-      // listen to sensor data
-      app.listen(thermometer);
-    }
+    device.sensorUrls.forEach(function (sensorUrl) {
+      app.listen(sensorDriver.createSensor(sensorUrl));
+    });
   });
 });
-
-var device = ble.createDevice('addr');
-device.connect();
-device.createSensor('sensorid');
