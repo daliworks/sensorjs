efaaaa75cb2ec58f9fed62341f4e7fcb059f0aef
Dowan Kim <forty4@gmail.com>
2014-08-18 20:16:43 +0900

    [example] add futuretek example to utilize sensorApp methods and change to load futuretek sensor package from node modules

--- example/future-sensor.js
+++ example/future-sensor.js
@@ -1,7 +1,11 @@
 'use strict';
 var connect = require('../'),
-   sensorDriver = connect.sensor,
-   future = sensorDriver.getNetwork('future');
+    sensorApp = connect.sensor,
+    futureSensor = require('sensorjs-futuretek'),
+    futureNetwork;
+
+sensorApp.addSensorPackage(futureSensor);
+futureNetwork = sensorApp.getNetwork('future');
 
 var app = connect().
   use('/future/*/futureTemp',function (data, next) { // custom middleware
@@ -14,10 +18,10 @@ var app = connect().
   }).
   use(connect.queue(100));                   // buffering max # of 100.
 
-future.discover(function (err, devices) {
+futureNetwork.discover(function (err, devices) {
   devices.forEach(function (device) {
     device.sensorUrls.forEach(function (sensorUrl) {
-      app.listen(sensorDriver.createSensor(sensorUrl));
+      app.listen(sensorApp.createSensor(sensorUrl));
     });
   });
 });
