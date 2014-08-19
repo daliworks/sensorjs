9a621dd2c63dfe7e624270b3b559a67a437f448a
Dowan Kim <forty4@gmail.com>
2014-08-19 10:10:17 +0900

    Update ble-acc.js

--- example/ble-acc.js
+++ example/ble-acc.js
@@ -1,7 +1,13 @@
 'use strict';
+
+// NOTE: sensorjs-ble is required (npm install sensorjs-ble)
 var connect = require('../'),
    sensorApp = connect.sensor,
-   ble = sensorApp.getNetwork('ble');
+   bleSensor = require('sensorjs-ble'),
+   ble;
+   
+sensorApp.addSensorPackage(bleSensor);
+ble = sensorApp.getNetwork('ble');
 
 var app = connect().
   use(function (data, next) {                // custom middleware
